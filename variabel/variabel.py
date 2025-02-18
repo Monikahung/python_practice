8b2f75c16c88f2643d87e92e4da3952505f6a6fd
Monikahung <monikahung580@gmail.com>
2025-02-19 00:44:46 +0700

    Upload variable

---
+++ variabel/variabel.py
@@ -0,0 +1,25 @@
+# variabel adalah tempat menyimpan data
+
+# assigment nilai
+a = 10 # artinya a bernilai 10
+x = 5 # artinya x bernilai 5
+panjang = 1000
+
+# pemanggilan pertama
+print("Nilai a = ", a)
+print("Nilai x = ", x)
+print("Nilai panjang = ", panjang)
+
+# penamaan
+nilai_y = 15 # penamaan dengan menggunakan underscore
+juta10 = 10000000 # ini boleh
+nilaiZ = 17.5 # ini boleh
+
+# pemanggilan kedua
+print("Nilai a = ", a)
+a = 7
+print("Nilai a = ", a)
+
+# assigment indirect
+b = a
+print("Nilai b = ", b)
